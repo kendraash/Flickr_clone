21509106b3935d449966b2211f478cce3503b861
Julianna Green & Kendra Ash <student@epicodus.com>
2015-10-21 11:18:20 -0700

    add validations and relationships to Comment class

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   has_attached_file :image
+  has_many :comments
+  belongs_to :user
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates_presence_of :caption
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 describe Post do
-  it { should have_attached_file(:image) }
+  it { should have_attached_file :image }
   it { should validate_presence_of :caption }
-
+  it { should belong_to :user}
+  it { should have_many :comments}
 end
